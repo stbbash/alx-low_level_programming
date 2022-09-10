8c8d2c87016d5c0ed80f2adbc2ebacf0ff36c010
Donatus Ajaezu <donatusajaezu@gmail.com>
2022-09-09 17:54:08 -0700

    three

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,12 @@
 #include <stdio.h> 
+/** 
+* main - entry point 
+* 
+* Return: alway returns 0 
+* 
+*/ 
 
- /** 
-
-  * main - entry point 
-
-  * 
-
-  * Return: alway returns 0 
-
-  * 
-
-  */ 
-
- int main(void) 
+int main(void) 
 
  { 
 
