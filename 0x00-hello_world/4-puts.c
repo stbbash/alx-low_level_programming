962e7ed5a2e5b752353376ce0c5530e9d5d0f3a7
Donatus Ajaezu <donatusajaezu@gmail.com>
2022-09-12 02:44:34 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,11 @@
 #include <stdio.h>
-  
 /** 
-
  * main - Entry point 
-
  * 
-
  * Return: always 0 
-
  */
-
-int main(void)
-  
+int main(void)  
 {
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
+  puts("\"Programming is like building a multilingual puzzle"); 
   return (0);
-  
 }
