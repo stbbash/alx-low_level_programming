72ce1a42af9c8835a0e19a3593f6b999d42713e6
Donatus Ajaezu <donatusajaezu@gmail.com>
2022-09-13 18:26:28 -0700

    ten

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,25 @@
+#include "main.h"
+  
+
+
+/** 
+
+ * add - function that adds two integers and returns the result 
+
+ * @n: first argument 
+
+ * @m: second argument 
+
+ * Return: the result of n plus m 
+
+ */
+
+
+
+int add(int n, int m)
+  
+{
+  
+  return (n + m);
+  
+}
