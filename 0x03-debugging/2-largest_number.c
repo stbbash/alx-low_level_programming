a11782eb9150493ca25de16e89c17ca40be86a17
Donatus Ajaezu <donatusajaezu@gmail.com>
2022-09-15 14:35:42 +0100

    two

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -9,17 +9,21 @@
 int largest_number(int a, int b, int c)
 {
   int largest;
-  if (a > b)
+  if (a > b && a > c)
     {
       largest = a;
+    }
+  else if (b > c && b > a)
+    {
+      largest = b;
+    }
+  else if (c > b)
+    {
+      largest = c;
     }
   else
     {
       largest = b;
-    }
-  if (c > largest)
-    {
-      largest = c;
     }
   return (largest); 
 }
