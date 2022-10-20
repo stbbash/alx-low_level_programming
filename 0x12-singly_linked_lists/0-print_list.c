b56a898c555e15db914ccbd167c9abb79010bac5
Donatus Ajaezu <donatusajaezu@gmail.com>
2022-10-20 14:56:33 -0700

    zero

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,20 +7,15 @@
 *Return: the number of nodes.
 */
 
-size_t print_list(const list_t *h)
+size_t print_listint(const listint_t *h)
 {
-	int count;
-
-	count = 0;
+	size_t count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-		printf("[0] (nil)\n");
-		else
-		printf("[%d] %s\n", h->len, h->str);
-		h = h->next;
+		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
